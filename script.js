5c1b2b00637098bb450fb6be2660b8830308e376
Tyson Jackson <tyson@alittlerdesigner>
2022-09-06 00:08:33 +0800

    Adding my if statments to define the character types chosen

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-//Variants of all charcter Arrays
+//Variables of all charcter Arrays
   var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
-//Variants of empty Arrays
+//Variables of empty Arrays
   var userArray = [];
   var resultArray = [];
 
@@ -20,7 +20,36 @@ function generatePassword() {
   var lowercases = confirm ("Would you like lowercases in your password?");
   var characters = confirm ("Would you like special characters in your password?");
 
+  //if you selected yes or no for the of the above confirm buttons it will pull the the from the top variables and store in resultArray for use later
+  if (numbers){
+    resultArray = resultArray.concat(numbArray);
+    
+  }
   
+  if (uppercases){
+    resultArray = resultArray.concat(uppercaseArray);
+  
+  }
+  
+  if (lowercases){
+    resultArray = resultArray.concat(lowercaseArray);
+  
+  }
+  
+  if (characters){
+    resultArray = resultArray.concat(characterArray);
+  }
+  //this will print the variables stored above from our if statments
+  console.log(resultArray)
+  
+  // this will use the Math functions of floor and random to randomly select characters from our top varibales and only the ones defined in the above if statements
+  for (var i = 0; i < numCharacter; i++) {
+        
+    userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+    }
+  
+    return userArray.join("") ;
+  }
 }
 
 // Write password to the #password input
